850c8954aee6f1cfcf3430745263b9e2447523a6
iamgriffon <guusilveira@gmail.com>
2023-08-18 22:13:39 -0300

    Commitando arquivos

---
+++ ex1/teste.ts
@@ -0,0 +1,10 @@
+import { soma, substrair, dividir, multiplicar } from "./hello";
+
+let a = 0;
+let b = 1;
+let c = 2;
+
+console.log(soma(a, b));
+console.log(substrair(b, c));
+console.log(multiplicar(c, c));
+console.log(dividir(b, a));
