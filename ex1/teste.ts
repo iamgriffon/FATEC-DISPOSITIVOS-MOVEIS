458be2e8e2ae5b55180bdf45d9dfbdfe5494055d
iamgriffon <guusilveira@gmail.com>
2023-08-18 22:18:42 -0300

    adicionando tipos e primeira operacao

--- ex1/teste.ts
+++ ex1/teste.ts
@@ -1,10 +1,9 @@
 import { soma, substrair, dividir, multiplicar } from "./hello";
 
+type MenuOptions = 'somar' | 'substrair' | 'dividir' | 'multiplicar'
+
 let a = 0;
 let b = 1;
 let c = 2;
 
 console.log(soma(a, b));
-console.log(substrair(b, c));
-console.log(multiplicar(c, c));
-console.log(dividir(b, a));
